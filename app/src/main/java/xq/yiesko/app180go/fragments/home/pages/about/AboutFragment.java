d7561f6982a1da98233548c4828e01e80c527eee
yieskoW <yiesko@proton.me>
2025-03-18 20:09:19 -0300

    feat(about): GitHub repository link button
    Change-Id: I063bfff1b18529ab6b224fc3a8956efbb28982fc
    Signed-off-by: yieskoW <yiesko@proton.me>

--- app/src/main/java/xq/yiesko/app180go/fragments/home/pages/about/AboutFragment.java
+++ app/src/main/java/xq/yiesko/app180go/fragments/home/pages/about/AboutFragment.java
@@ -1,6 +1,9 @@
 package xq.yiesko.app180go.fragments.home.pages.about;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import xq.yiesko.app180go.R;
 import xq.yiesko.app180go.databinding.FragmentAboutBinding;
@@ -37,5 +42,19 @@ public class AboutFragment extends Fragment {
         } catch (PackageManager.NameNotFoundException e) {
             fragmentAboutBinding.aboutFragmentVersionText.setText(R.string.about_fragment_version_unknown);
         }
+
+        fragmentAboutBinding.aboutFragmentBtnGithub.setOnClickListener(v -> {
+            try {
+                var intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://github.com/yiesko/180go"));
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Snackbar.make(fragmentAboutBinding.getRoot(),
+                                R.string.about_fragment_view_source_browser_not_found,
+                                Snackbar.LENGTH_LONG)
+                        .setAction(android.R.string.ok, null)
+                        .show();
+            }
+        });
     }
 }
